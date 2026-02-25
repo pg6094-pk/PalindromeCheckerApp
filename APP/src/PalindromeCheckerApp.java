12207fda1f5e401f929b6cdac104a1f940d8bab1
rachita <rd1363@srmist.edu.in>
2026-02-25 15:14:11 +0530

    2

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,26 +1,8 @@
-import java.util.Scanner;
-
-public class UseCase3PalindromeCheckerApp {
-
+public class PalindromeCheckerApp  {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter a string: ");
-        String input = scanner.nextLine();
-
-        String reversed = "";
-
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed = reversed + input.charAt(i);
-        }
-
-        if (input.equals(reversed)) {
-            System.out.println("Result: The string is a Palindrome.");
-        } else {
-            System.out.println("Result: The string is NOT a Palindrome.");
-        }
-
-        scanner.close();
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
     }
 }
+
