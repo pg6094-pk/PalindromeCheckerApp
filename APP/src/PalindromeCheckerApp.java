a3a87c10cbf7cda3389a0523ddc88c1adc241aba
pg6094-pk <pg6094@srmist.edu.in>
2026-03-12 15:45:45 +0530

    UC10: Case-Insensitive & Space-Ignored Palindrome

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,26 +1,28 @@
-import java.util.LinkedList;
+import java.util.Scanner;
 
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        String input = "level";
 
-        LinkedList<Character> list = new LinkedList<>();
+        Scanner sc = new Scanner(System.in);
 
-        for (char c : input.toCharArray()) {
-            list.add(c);
-        }
+        System.out.print("Input : ");
+        String input = sc.nextLine();
+
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
         boolean isPalindrome = true;
 
-        while (list.size() > 1) {
-            if (list.removeFirst() != list.removeLast()) {
+        for (int i = 0; i < normalized.length() / 2; i++) {
+
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        System.out.println("Input       : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+
+        sc.close();
     }
 }
